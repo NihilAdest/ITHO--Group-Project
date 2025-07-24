7a4d2b795f815f59a183fd62b672dbe18ef78fcf
Brannon Lown <blown@charlotte.edu>
2025-07-24 11:12:43 -0400

    feat: customer model, upd orders

--- api/models/orders.py
+++ api/models/orders.py
@@ -8,8 +8,13 @@ class Order(Base):
     __tablename__ = "orders"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    customer_name = Column(String(100))
+    customer_name = Column(String, ForeignKey("customers.name"))
     order_date = Column(DATETIME, nullable=False, server_default=str(datetime.now()))
+    tracking_id = Column(String(300), nullable=False)
     description = Column(String(300))
+    status = Column(String(300))
+    total_price = Column(DECIMAL(10,2))
+    customer_id = Column(Integer, ForeignKey("customers.id"))
 
-    order_details = relationship("OrderDetail", back_populates="order")
+    order_details = relationship("OrderDetail", back_populates="orders")
+    customer = relationship("Customer", back_populates="orders")
