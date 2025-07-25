b5585129d688f8c3ce9c578b3e2d62983c3a74c4
Kylee <kyleekeithh@gmail.com>
2025-07-24 22:51:16 -0500

    feat: added resource management model

---
+++ api/models/resource_management.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, DATETIME
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from ..dependencies.database import Base
+
+class ResourceManagement(Base):
+    __tablename__ = "resource_management"
+
+    ingredient_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String)
+    unit = Column(String)
+    current_stock = Column(DECIMAL(10,2))
+    min_stock_level = Column(DECIMAL(10,2))
+    cost_per_unit = Column(DECIMAL(10,2))
+    restaurant_id = Column(Integer, ForeignKey("restaurants.restaurant_id"))
+
+    restaurant = relationship("Restaurant", back_populates="resources")
